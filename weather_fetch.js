483e5d06bfca62f061ffbe7c506d8ea9b696880a
Tom Wallett <tomwallett11@gmail.com>
2026-03-15 11:37:01 +1000

    remove files from root

--- weather_fetch.js
+++
@@ -1,9 +0,0 @@
-async function getFirebaseData() {
-    const response = await fetch("https://arduino-weather-station-553f2-default-rtdb.firebaseio.com/readings.json")
-    const data = await response.json()
-    // update your HTML elements here
-    document.getElementById("temperature").innerText = data.temperature
-    document.getElementById("humidity").innerText = data.humidity
-}
-getFirebaseData()
-setInterval(getFirebaseData, 5000)
